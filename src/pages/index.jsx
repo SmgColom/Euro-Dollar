e8d18e6828d92b2f4d6e17100b9c411b38eddff2
Santiago Montoya <158979737+SmgColom@users.noreply.github.com>
2025-06-05 12:40:39 +1000

    SEO improvement

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,9 +8,16 @@ function HomePage() {
        <Head>
         <title>SegurosMileToro | Home</title>
         <meta 
-          name='Seguros Mile Toro '
+          name='description'
           description='Aquí encuentras las soluciones de seguros a tu medida con el mejor respaldo de Sura'
           />
+
+                  {/* Meta etiquetas Open Graph (SEO y redes sociales) */}
+        <meta property="og:title" content="SegurosMileToro | Home" />
+        <meta property="og:description" content="Aquí encuentras las mejores soluciones de seguros con el respaldo de Sura." />
+        <meta property="og:image" content="https://miletoroseguros.vercel.app/tigre.jpg" />
+        <meta property="og:url" content="https://miletoroseguros.vercel.app/" />
+        <meta property="og:type" content="website" />
           </Head>
       <HeroHome 
       title="Proteccion personal y patrimonial para toda la familia"
