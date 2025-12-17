cd42bc00620adb89fe011733e67e4547815c9c96
Santiago Montoya <158979737+SmgColom@users.noreply.github.com>
2025-12-17 18:44:42 -0500

    add sitemap and robots

---
+++ src/next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://eurodollarcolombia.com',
+  generateRobotsTxt: true, // genera robots.txt automáticamente
+  sitemapSize: 7000,
+  changefreq: 'daily',
+  priority: 1.0,
+};
