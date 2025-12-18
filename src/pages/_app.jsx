a4b9b0f2f5e33012f26b7d9847a308cb35b25ae3
Santiago Montoya <158979737+SmgColom@users.noreply.github.com>
2025-12-18 00:16:44 -0500

    add GA4 google tag

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -49,6 +49,26 @@ function App({ Component, pageProps }) {
         }}
       />
 
+      <Script
+  strategy="afterInteractive"
+  src="https://www.googletagmanager.com/gtag/js?id=G-LL90ZEJSB9"
+/>
+
+<Script
+  id="ga4-init"
+  strategy="afterInteractive"
+>
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-LL90ZEJSB9', {
+      page_path: window.location.pathname,
+    });
+  `}
+</Script>
+
+
       <Head>
         <link
           rel="icon"
